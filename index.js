2ff6dcc897a1967bc7c6786b28895106a8e0e771
marnixah <marnix@roboco.dev>
2023-06-15 11:53:28 +0200

    accept all routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   const iterations = req.query.iterations;
   const timer = Math.random();
   console.time(`iterations-${iterations}-${timer}`);
